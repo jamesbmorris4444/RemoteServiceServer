fa815144f71cdbbf7c08f47e2a697f0b70502285
jmorris <jim.morris@mobilepointofsales.com>
2020-03-23 20:34:15 -0500

    Initial commit

---
+++ app/src/main/java/com/fullsekurity/remoteserviceserver/BoundService.kt
@@ -0,0 +1,68 @@
+package com.fullsekurity.remoteserviceserver
+
+import android.app.Service
+import android.content.Intent
+import android.os.*
+import android.widget.Chronometer
+import java.lang.ref.WeakReference
+
+class BoundService : Service() {
+    private var mChronometer: Chronometer? = null
+    private val mMessenger = Messenger(BoundServiceHandler(this))
+
+    internal class BoundServiceHandler(service: BoundService) : Handler() {
+        private val mService = WeakReference(service)
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                MSG_GET_TIMESTAMP -> {
+                    mService.get()?. let {
+                        val elapsedMillis = (SystemClock.elapsedRealtime() - mService.get()!!.mChronometer!!.base)
+                        val hours = (elapsedMillis / 3600000).toInt()
+                        val minutes = (elapsedMillis - hours * 3600000).toInt() / 60000
+                        val seconds = (elapsedMillis - hours * 3600000 - (minutes * 60000)).toInt() / 1000
+                        val millis = (elapsedMillis - hours * 3600000 - (minutes * 60000) - seconds * 1000).toInt()
+                        val activityMessenger = msg.replyTo
+                        val b = Bundle()
+                        b.putString("timestamp", "$hours:$minutes:$seconds:$millis")
+                        val replyMsg = Message.obtain(null, MSG_GET_TIMESTAMP)
+                        replyMsg.data = b
+                        try {
+                            activityMessenger.send(replyMsg)
+                        } catch (e: RemoteException) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+                else -> super.handleMessage(msg)
+            }
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mChronometer = Chronometer(this)
+        mChronometer?.base = SystemClock.elapsedRealtime()
+        mChronometer?.start()
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return mMessenger.binder
+    }
+
+    override fun onRebind(intent: Intent) {
+        super.onRebind(intent)
+    }
+
+    override fun onUnbind(intent: Intent): Boolean {
+        return true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mChronometer?.stop()
+    }
+
+    companion object {
+        const val MSG_GET_TIMESTAMP = 1000
+    }
+}
---
+++ app/src/main/java/com/fullsekurity/remoteserviceserver/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.fullsekurity.remoteserviceserver
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.*
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import java.lang.ref.WeakReference
+
+class MainActivity : AppCompatActivity() {
+    private var mBoundServiceMessenger: Messenger? = null
+    private var mServiceConnected = false
+    private var mTimestampText: TextView? = null
+    private val mActivityMessenger = Messenger(
+            ActivityHandler(this))
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mTimestampText = findViewById<View>(R.id.timestamp_text) as TextView
+        val printTimestampButton = findViewById<View>(R.id.print_timestamp) as Button
+        val stopServiceButon = findViewById<View>(R.id.stop_service) as Button
+        val startServiceButon = findViewById<View>(R.id.start_service) as Button
+
+        startServiceButon.setOnClickListener {
+            val intent = Intent(this@MainActivity, BoundService::class.java)
+            startService(intent)
+            bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE)
+        }
+        printTimestampButton.setOnClickListener {
+            if (mServiceConnected) {
+                try {
+                    val msg = Message.obtain(null,
+                            BoundService.MSG_GET_TIMESTAMP, 0, 0)
+                    msg.replyTo = mActivityMessenger
+                    mBoundServiceMessenger!!.send(msg)
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        stopServiceButon.setOnClickListener {
+            if (mServiceConnected) {
+                unbindService(mServiceConnection)
+                mServiceConnected = false
+            }
+            val intent = Intent(this@MainActivity,
+                    BoundService::class.java)
+            stopService(intent)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mServiceConnected) {
+            unbindService(mServiceConnection)
+            mServiceConnected = false
+        }
+    }
+
+    private val mServiceConnection: ServiceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName) {
+            mBoundServiceMessenger = null
+            mServiceConnected = false
+        }
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            mBoundServiceMessenger = Messenger(service)
+            mServiceConnected = true
+        }
+    }
+
+    internal class ActivityHandler(activity: MainActivity) : Handler() {
+        private val mActivity = WeakReference(activity)
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                BoundService.MSG_GET_TIMESTAMP -> {
+                    mActivity.get()?.mTimestampText?.text = msg.data.getString("timestamp")}
+            }
+        }
+    }
+}
